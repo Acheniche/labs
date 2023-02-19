ed7c29f38109dea2b3fe04cd247d2763dd160a0e
Acheniche <99502683+Acheniche@users.noreply.github.com>
2023-02-19 20:20:13 +0300

    feet: add playList

---
+++ playList.js
@@ -0,0 +1,23 @@
+const playList = [
+    {      
+      title: 'Animals',
+      src: 'https://github.com/Acheniche/labs/blob/gh-pages/playList/Architects-Animals.mp3',
+      duration: '04:04'
+    },  
+    {      
+      title: 'The Worst In Me',
+      src: 'https://github.com/Acheniche/labs/blob/gh-pages/playList/Bad%20Omens-The%20Worst%20In%20Me.mp3',
+      duration: '03:49'
+    },
+    {      
+        title: 'Wrong Side of Heaven',
+        src: 'https://github.com/Acheniche/labs/blob/gh-pages/playList/Five%20Finger%20Death%20Punch-Wrong%20Side%20of%20Heaven.mp3',
+        duration: '04:41'
+      },  
+      {      
+        title: 'MONSTERS',
+        src: 'https://github.com/Acheniche/labs/blob/gh-pages/playList/Shinedown-MONSTERS.mp3',
+        duration: '04:08'
+      },  
+  ]
+  export default playList;
